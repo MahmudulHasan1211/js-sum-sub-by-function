ec88bb0b5bd39e5f841c4256aebe65ff81df5518
mahmudul-hasan <mhmahmudul1211@gmail.com>
2024-09-10 14:38:53 +0600

    first commit

---
+++ sum by function.js
@@ -0,0 +1,16 @@
+// sum by function 
+function sum(x,y){
+    const result = x + y;
+    return result;
+}
+    const total = sum (10,15)
+    console.log(total)
+
+
+// subtraction by function 
+function sub (x,y){
+    const result = x-y;
+    return result
+}
+    const subtraction = sub(999,99)
+    console.log(subtraction)
